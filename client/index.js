7f997d41105a0af0b7dff7eae0ccb43c419e1916
Joe Contreras <joe.contreras888@gmail.com>
2022-08-17 14:13:46 -0500

    Working version with apollo in the frontend

--- client/index.js
+++ client/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "./style/style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-client";
+import SongList from "./Components/SongList";
+import { hashHistory, IndexRoute, Route, Router } from "react-router";
+import App from "./Components/App";
+import SongCreate from "./Components/SongCreate";
+import SongDetail from "./Components/SongDetail";
+
+const client = new ApolloClient({
+  dataIdFromObject: (o) => o.id,
+});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
